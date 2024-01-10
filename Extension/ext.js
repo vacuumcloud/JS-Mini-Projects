41ea6e5b08e375615d082df6ac733b7104992d55
vacuumcloud <udaychandrulc@gmail.com>
2024-01-10 23:33:06 +0530

    finished lead tracker v1

--- Extension/ext.js
+++ Extension/ext.js
@@ -1,28 +1,51 @@
 const inputBtn = document.querySelector("#input-btn")
+const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 
 let myLeads = []
 const inputEl = document.querySelector("#input-el")
 const inputPara = document.querySelector(".leads-p")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-localStorage.clear()
+if(leadsFromLocalStorage)
+{
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    renderLeads()
-    inputEl.value = ""
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for(let i = 0; i< myLeads.length; i++)
+    for(let i = 0; i< leads.length; i++)
     {
         listItems += `
         <li>
-            <a target= '_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
+            <a target= '_blank' href='${leads[i]}'>${leads[i]}</a>
         </li>`
     }
     inputPara.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("click", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
 
 
 
